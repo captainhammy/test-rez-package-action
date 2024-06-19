55ace3fa4cd55d56387d7b701f7ae7077d2f84b1
Graham Thompson <captainhammy@gmail.com>
2024-06-19 10:33:38 -0700

    test: more debug output

--- tests/test_package/package.py
+++ tests/test_package/package.py
@@ -8,7 +8,7 @@ build_system = "cmake"
 # Pass -c to pytest, so it doesn't try and load the pyproject.toml from the parent folder
 tests = {
     "unit": {"command": "python -m pytest tests/", "requires": ["pytest"]},
-    "specific": {"command": "python -m pytest -c tests/", "requires": ["pytest"]},
+    "specific": {"command": "python -m pytest tests/", "requires": ["pytest"]},
 }
 
 
--- tests/test_package/tests/test_file.py
+++ tests/test_package/tests/test_file.py
@@ -5,15 +5,9 @@ import os
 import pathlib
 
 
-def test_set_output():
-    """Write to output data."""
-    output_path = pathlib.Path(os.environ["GITHUB_OUTPUT"])
-    print(output_path.as_posix())
-
-    with output_path.open("a", encoding="utf-8") as fp:
-        fp.write("package_test_ran=1\n")
-
-    print(f"Wrote output to {output_path.as_posix()}")
-
-    with output_path.open("r", encoding="utf-8") as fp:
-        print(fp.read())
+# def test_set_output():
+#     """Write to output data."""
+#     output_path = pathlib.Path(os.environ["GITHUB_OUTPUT"])
+#
+#     with output_path.open("a", encoding="utf-8") as fp:
+#         fp.write("package_test_ran=1\n")
