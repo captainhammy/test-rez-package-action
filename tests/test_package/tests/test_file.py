527e5ae6afafbc7ce743212009bbc95249543ae1
Graham Thompson <captainhammy@gmail.com>
2024-06-19 09:32:19 -0700

    test: more debug output

--- tests/test_package/tests/test_file.py
+++ tests/test_package/tests/test_file.py
@@ -8,6 +8,9 @@ import pathlib
 def test_set_output():
     """Write to output data."""
     output_path = pathlib.Path(os.environ["GITHUB_OUTPUT"])
+    print(output_path.as_posix())
 
     with output_path.open("a", encoding="utf-8") as fp:
         fp.write("package_test_ran=1\n")
+
+    print(f"Wrote output to {output_path.as_posix()}")
