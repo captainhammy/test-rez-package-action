80f43a4d87c4db4de11796606d38a0621a167032
Graham Thompson <captainhammy@gmail.com>
2024-08-10 07:16:18 -0700

    ensure target directory exists

--- tests/test_package/tests/test_specific.py
+++ tests/test_package/tests/test_specific.py
@@ -12,6 +12,7 @@ def test_set_output():
     itself.
     """
     output_path = pathlib.Path(os.environ["TEST_TEMP_DIR"]) / "specific.txt"
+    output_path.parent.mkdir(parents=True, exist_ok=True)
 
     with output_path.open("a", encoding="utf-8") as fp:
         fp.write("package_test_ran=1\n")
